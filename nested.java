6b9cded56a31ec77248c9cb7dc049c794cede787
Pablo Hernandez <hpablo575@gmail.com>
2022-12-17 10:59:54 -0700

    cereated nested if statement and compiled program

--- nested.java
+++ nested.java
@@ -3,5 +3,41 @@ import java.util.Scanner;
 public class nested {
     public static void main (String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter temp");
+
+        int temp = input.nextInt();
+
+        
+
+        //hot temps
+
+        if(temp >= 80) {
+            if(temp >= 110) {
+                System.out.println("dont go out there bruh");
+            }
+            else {
+                System.out.println("wear shorts big dawg");
+            }
+
+
+      
+        }
+
+        //cold temps
+        else {
+            if(temp <= 30) {
+                System.out.println("dont do it bro");
+
+
+            }
+            else {
+                System.out.println("wear pants and jacket");
+            }
+        }
+       
+
+
+
     }
 }
